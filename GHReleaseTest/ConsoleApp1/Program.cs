96393e964ada56115ad22c9086866f00ffdcc56b
ingen084 <ingen188@gmail.com>
2022-01-04 12:41:22 +0900

    プロジェクト追加

---
+++ GHReleaseTest/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("this app version: " + Assembly.GetExecutingAssembly().GetName().Version);
